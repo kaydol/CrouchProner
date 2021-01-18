e54d2badeb548c83d9fd2e61380d7a92161337f0
kaydol <volyaandrazum@mail.ru>
2021-01-19 09:08:26 +1000

    Added .bat file

---
+++ Release/StartCrouchProner.bat
@@ -0,0 +1,3 @@
+:: First number is HOLD_TIME (how much to hold the trigger until the RESPONSE is pressed)
+:: Second number is how many times to check whether the trigger is still held during HOLD_TIME
+CrouchProner 400 15
